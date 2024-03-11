2c197fa0db1de8e7e20034a59993844a7b2e8192
tam11a <ibrahimsadiktamim@gmail.com>
2024-03-11 15:06:03 +0600

    removed build folder

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import * as stringHelpers from './lib/string-helper';
-import * as arrayObjectHelpers from './lib/array-object-helper';
-import * as dateTimeHelpers from './lib/datetime-helper';
-export default {
-    stringHelpers,
-    arrayObjectHelpers,
-    dateTimeHelpers,
-};
